799ba27df17bce1367c64415ca1e14ccbe08c3ca
Sugapriyanavamaniyahoo <40208736+Sugapriyanavamaniyahoo@users.noreply.github.com>
2023-08-31 22:35:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,9 @@ number2 = st.number_input('Enter Second Numner: ')
 
 number3 = st.number_input('Enter Third Numner: ')
 
+if (number1 > number2)
+st.write("First Number is largest")
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
